305d51dffc1cf5d2cfebc933346276a4faa1bf39
Fernando Morais <fernandouitec@gmail.com>
2026-06-05 19:03:35 -0300

    fix(deploy): corrige path ../backend em api/index.php

--- frontend/public/api/index.php
+++ frontend/public/api/index.php
@@ -8,4 +8,4 @@
  */
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['PHP_SELF']    = '/index.php';
-require __DIR__ . '/../../backend/public/index.php';
+require __DIR__ . '/../backend/public/index.php';
